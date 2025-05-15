906feb4a346467d93c195e7e73fc6e9e1f7f5adb
楠木 <zyhnanmu@gmail.com>
2025-05-15 17:26:01 +0800

    优化分类组件，修复分类ID引用，增强右键菜单功能，完善弹窗确认逻辑

--- src/components/Classification.vue
+++ src/components/Classification.vue
@@ -10,44 +10,121 @@
       />
       <input
         v-for="category in categorys"
-        :key="category.id"
+        :key="category._id"
         type="button"
-        :data-id="category.id"
+        :data-id="category._id"
         class="btn btn-label btn-allFinish btn-category"
         :value="category.title"
-        :class="{ action: selectedCategory == category.id }"
-        @click="selectedCategory = category.id"
+        :class="{ action: selectedCategory == category._id }"
+        @click="selectedCategory = category._id"
       />
     </div>
   </ContextMenu>
+  <Dialog
+    ref="dialog"
+    :title="fromData.categoryId ? '修改分类' : '添加分类'"
+    @confirm="handleConfirm"
+    @close="handleClose"
+  >
+    <template #content>
+      <input
+        type="text"
+        class="custom-alert-input"
+        v-model="fromData.title"
+        placeholder="分类名"
+        ref="inputEl"
+      />
+    </template>
+  </Dialog>
 </template>
 
 <script setup>
 import Dialog from "@/common/Dialog.vue";
+import { showMessageBox } from "@/utils/MessageBox.js";
+import { del, get, update, create } from "@/api/classification";
 import ContextMenu from "@/common/ContextMenu/ContextMenu.vue";
 import { useDataStore } from "@/stores/userStore.js";
 import { storeToRefs } from "pinia";
+import { ref, onMounted, nextTick } from "vue";
 
 const store = useDataStore();
 const { categorys, selectedCategory } = storeToRefs(store);
+const inputEl = ref(null);
+onMounted(() => {
+  getMenu();
+});
+
+const getMenu = () => {
+  get().then((res) => {
+    categorys.value = res.data.list;
+  });
+};
+
+const dialog = ref(null);
+const fromData = ref({
+  categoryId: "",
+  title: "",
+  sort: "",
+});
+const handleClose = () => {
+  fromData.value = {
+    categoryId: "",
+    title: "",
+    sort: "",
+  };
+};
+// 弹窗确认
+const handleConfirm = () => {
+  if (fromData.value.categoryId) {
+    update(fromData.value).then((res) => {
+      if (res.errCode == 0) {
+        showMessageBox("🎉修改成功!", "成功");
+      } else {
+        showMessageBox(" 😅修改失败!", "失败");
+      }
+    });
+  } else {
+    create(fromData.value).then((res) => {
+      if (res.errCode == 0) {
+        showMessageBox("🎉添加成功!", "成功");
+      } else {
+        showMessageBox(" 😅添加失败!", "失败");
+      }
+    });
+  }
+  dialog.value.switchShow();
+  getMenu();
+};
 //右键
 const handleContextMenu = (menuItem, element) => {
   console.log("右键元素:", element.value); // 获取右键点击的 DOM 元素
   console.log(menuItem);
-  // switch (menuItem.label) {
-  //   case "添加":
-  //     addTodo();
-  //     break;
-  //   case "修改":
-  //     // 通过 DOM 元素定位数据（例如：element.dataset.id）
-  //     const todoId = element.dataset.id;
-  //     const todo = todos.value.find((t) => t.id === parseInt(todoId));
-  //     if (todo) editdTodo(todo);
-  //     break;
-  //   case "删除":
-  //     const deleteId = element.dataset.id;
-  //     store.removeTodo(deleteId);
-  //     break;
-  // }
+  switch (menuItem.label) {
+    case "添加":
+      dialog.value.switchShow();
+
+      break;
+    case "修改":
+      dialog.value.switchShow();
+      fromData.value.title = categorys.value.find(
+        (item) => item._id === element.value
+      ).title;
+      fromData.value.categoryId = element.value;
+
+      break;
+    case "删除":
+      fromData.value.categoryId = element.value;
+      showMessageBox("确认删除吗？", "提示").then(() => {
+        del(fromData.value).then((res) => {
+          if (res.errCode == 0) {
+            getMenu();
+          }
+        });
+      });
+      break;
+  }
+  nextTick(() => {
+    if (inputEl.value) inputEl.value.focus();
+  });
 };
 </script>
