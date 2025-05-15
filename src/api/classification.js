4aac09f0150771cb2567c0fa4c98750b49fa1de8
楠木 <zyhnanmu@gmail.com>
2025-05-15 11:48:04 +0800

    新增分类API接口，包括创建、更新、查询和删除功能

---
+++ src/api/classification.js
@@ -0,0 +1,18 @@
+import axios from "@/axios.js";
+
+// 添加分类
+export function create(data) {
+  return axios.post("/classification/create", data);
+}
+//更新
+export function update(data) {
+  return axios.post("/classification/update", data);
+}
+//查询
+export function get(data) {
+  return axios.post("/classification/get", data);
+}
+//删除
+export function del(data) {
+  return axios.post("/classification/del", data);
+}
